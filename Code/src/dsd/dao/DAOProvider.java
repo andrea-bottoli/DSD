806e249b194d81f758d6d905470219af5891d198
Jörn Tillmanns <github@matedealer.de>
2013-11-19 20:12:18 +0100

    working on the Picture-Stuff implement secure Select Methode

--- Code/src/dsd/dao/DAOProvider.java
+++ Code/src/dsd/dao/DAOProvider.java
@@ -20,6 +20,10 @@ public class DAOProvider
 
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public static DataSource getDataSource()
 	{
 		try
@@ -37,6 +41,13 @@ public class DAOProvider
 		return dataSource;
 	}
 
+	/**
+	 * 
+	 * @param query
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
 	public static int Query(String query, Connection con) throws SQLException
 	{
 		try
@@ -51,6 +62,16 @@ public class DAOProvider
 		return 0;
 	}
 
+	/**
+	 * 
+	 * @param table
+	 * @param select
+	 * @param where
+	 * @param order
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
 	public static ResultSet SelectTable(String table, String select, String where, String order,
 			Connection con) throws SQLException
 	{
@@ -70,6 +91,59 @@ public class DAOProvider
 		return resultSet;
 	}
 
+	/**
+	 * This is the secure Version of SelectTable. To use this function in the
+	 * right way, you use ? instead of parameters and give the parameters
+	 * instead as string into the parameter-array.
+	 * 
+	 * For Example: Instead of : "SELECT * FROM tb1 WHERE user= 'foo' and name
+	 * ='bar' use "SELECT * FROM tb1 WHERE user= ? and name = ?" and as
+	 * parameters[0]='foo' and parameters[1]='bar'
+	 * 
+	 * The order in the parameters are important!!
+	 * 
+	 * @param table
+	 * @param select
+	 * @param where
+	 * @param order
+	 * @param con
+	 * @param parameters
+	 * @return
+	 * @throws SQLException
+	 */
+	public static ResultSet SelectTableSecure(String table, String select, String where, String order,
+			Connection con, String[] parameters) throws SQLException
+	{
+		ResultSet resultSet = null;
+		try
+		{
+			PreparedStatement command = con.prepareStatement(String.format("select %s from %s %s %s", select,
+					table, (where.trim().equals("") ? "" : "where " + where), (order.trim().equals("")
+							? ""
+							: "order by " + order)));
+
+			for (int i = 0; i < parameters.length; i++)
+			{
+				command.setString(i, parameters[i]);
+			}
+
+			resultSet = command.executeQuery();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return resultSet;
+	}
+
+	/**
+	 * 
+	 * @param table
+	 * @param where
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
 	public static int DeleteRow(String table, String where, Connection con) throws SQLException
 	{
 		try
@@ -85,6 +159,15 @@ public class DAOProvider
 		return 0;
 	}
 
+	/**
+	 * 
+	 * @param table
+	 * @param fields
+	 * @param values
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
 	public static int InsertRow(String table, String fields, String values, Connection con)
 			throws SQLException
 	{
@@ -107,6 +190,15 @@ public class DAOProvider
 		return 0;
 	}
 
+	/**
+	 * 
+	 * @param table
+	 * @param set
+	 * @param where
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
 	public static int UpdateRow(String table, String set, String where, Connection con) throws SQLException
 	{
 		try
--- Code/src/dsd/dao/PicturesDAO.java
+++ Code/src/dsd/dao/PicturesDAO.java
@@ -1,5 +1,85 @@
 package dsd.dao;
 
-public class PicturesDAO {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import dsd.model.Picture;
+
+public class PicturesDAO
+{
+
+	private static String tableName = "pictures";
+	private static String[] tableFields = new String[]{"ID", "path", "timestamp", "camera"};
+
+	/***
+	 * Returns all Pictures between given start- and end-Date from DB
+	 * 
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 */
+	public static ArrayList<Picture> getPictures(long startDate, long endDate)
+	{
+		ArrayList<Picture> returnList = new ArrayList<Picture>();
+
+		try
+		{
+			Connection con = DAOProvider.getDataSource().getConnection();
+			ResultSet result = DAOProvider.SelectTable(tableName, "*", "timestamp > " + startDate
+					+ " and timestamp <" + endDate, "", con);
+
+			while (result.next())
+			{
+				Picture pic = new Picture();
+				pic.setID(result.getInt(tableFields[0]));
+				pic.setPath(result.getString(tableFields[1]));
+				pic.setTimestamp(result.getLong(tableFields[2]));
+				pic.setCamera(result.getInt(tableFields[3]));
+				returnList.add(pic);
+			}
+
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return returnList;
+	}
+
+	/***
+	 * Write all pictures from the given list into DB
+	 * 
+	 * @param pictureList
+	 */
+	public void setPictures(ArrayList<Picture> pictureList)
+	{
+
+		Iterator<Picture> pictureIterator = pictureList.iterator();
+		try
+		{
+			Connection con = dsd.dao.DAOProvider.getDataSource().getConnection();
+			while (pictureIterator.hasNext())
+			{
+				Picture pic = pictureIterator.next();
+
+				// Use not a secure function in the moment!!!
+				String input = pic.getPath() + ", " + pic.getTimestamp() + ", " + pic.getCamera();
+				DAOProvider.InsertRow(tableName, tableFields[1] + ", " + tableFields[2] + ", "
+						+ tableFields[3], input, con);
+
+			}
+
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
