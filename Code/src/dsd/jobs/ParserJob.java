c8eab83127b754acaa6a2f662e58bdc2a072af4b
Lorenzo Pagliari <lorenzo.pagliari@hotmail.it>
2013-11-22 14:12:20 +0100

    test

--- Code/src/dsd/jobs/ParserJob.java
+++ Code/src/dsd/jobs/ParserJob.java
@@ -4,15 +4,14 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import dsd.controller.ParserControler;
 import dsd.controller.StartParser;
 
 public class ParserJob implements Job{
 
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		StartParser.CheckAndStart();
-		System.out.println("QUARTZ JOB TEST EVERY 5 seconds");
+		System.out.println("QUARTZ JOB TEST EVERY 15 seconds");
+		//StartParser.CheckAndStart();
 	}
 
 }
