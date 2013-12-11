341c9aeea7d2ad38a417050acc771fb645a9c37b
Dzana Kujan <dzana.kujan@gmail.com>
2013-12-11 02:21:11 +0100

    Implemented history range choice. Added jquery and jquery UI. Still some fixes needed, especially refactoring.

---
+++ Code/WebContent/JS/validations.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+function validateRange()
+{
+
+	var errorFlag = false;
+	var alertString = "Please insert the following parameters:";
+
+	var startDate = document.forms["dateRange"]["from"].value;
+	if (startDate == "") {
+		alertString += "\n*Start date";
+		errorFlag = true;
+	}
+	var endDate  = document.forms["dateRange"]["to"].value;
+	if (endDate == "") {
+		alertString += "\n*End date";
+		errorFlag = true;
+	}
+	if (errorFlag == true) {
+		alert(alertString);
+		return false;
+	}
+}
