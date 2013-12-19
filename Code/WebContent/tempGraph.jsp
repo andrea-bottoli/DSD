28308a0f6d0eeec08eb8554b06554b10fdffdd3d
Jörn Tillmanns <github@matedealer.de>
2013-12-19 08:38:06 +0100

    first included Version of rescue Table, without user-limitation

--- Code/WebContent/tempGraph.jsp
+++ Code/WebContent/tempGraph.jsp
@@ -1,10 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page import="dsd.model.WorstPylonCase"%>
+<%@page import="dsd.model.calculation.Pylon"%>
 <!DOCTYPE html>
 
 <t:desktopPage>
+	<jsp:useBean id="wcPylonArray" type="java.util.ArrayList<dsd.model.WorstPylonCase>" scope="request"/>
 
-	<body>
+	
 		<div >
 			<div id="actual_values">
 				<h3>Actual Values</h3>
@@ -476,10 +480,39 @@
 
 				</div>
 		
+		<div id="parameters">
+		<pclass="graph_name" > Worst-Case-Table</p>
+		<table class="hoverTable">
+			<tr>
+				<td  style="width:5%; font-weight:900; font-size:100%">Pylon ID</td>
+				<td  style="width:10%; font-weight:bold">Worst Case</td>
+				<td  style="width:20%; font-weight:bold">Combination number/lable</td>
+				<td  style="width:10%; font-weight:bold">N</td>
+				<td  style="width:5%; font-weight:bold">M</td>
+				<td  style="width:10%; font-weight:bold">Tx</td>
+				<td style="width:5%; font-weight:bold">Ty</td>
+				<td  style="width:10%; font-weight:bold">Mx</td>
+				<td  style="width:20%; font-weight:bold">My</td>
+			</tr>
+			<c:forEach items="${wcPylonArray}" var="item">
+				<tr>
+						<td >${item.pylonNumber}</td>	
+						<td >${item.safetyFactor}</td>
+						<td >${item.comboNumber}</td>	
+						<td >${item.n}</td>
+						<td >${item.m}</td>	
+						<td >${item.tx}</td>	
+						<td >${item.ty}</td>	
+						<td >${item.mx}</td>	
+						<td >${item.my}</td>			
+							
+				</tr>
+			</c:forEach>
+		</table>
+	</div>
 			 
 			<div class="clear_float"></div>
 		
 
-	</body>
 
 </t:desktopPage>
--- Code/src/dsd/view/CurrentStateView.java
+++ Code/src/dsd/view/CurrentStateView.java
@@ -17,8 +17,10 @@ import org.json.JSONObject;
 import dsd.controller.CalculatedDataController;
 import dsd.controller.ParsedInputFilesController;
 import dsd.controller.RawDataController;
+import dsd.controller.WorstCaseController;
 import dsd.model.CalculatedData;
 import dsd.model.RawData;
+import dsd.model.WorstPylonCase;
 import dsd.model.enums.eCalculatedDataType;
 import dsd.model.enums.eFileType;
 
@@ -27,72 +29,84 @@ public class CurrentStateView extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+
 		Calendar calStart = Calendar.getInstance();
-		calStart.set(2011, 2, 22, 16, 46, 0);//2011-03-23 16:46:00
-		
+		calStart.set(2011, 2, 25, 17, 00, 01);// 2011-03-23 16:46:00
+
 		Calendar calEnd = Calendar.getInstance();
-		calEnd.set(2011, 2, 23, 16, 56, 30);//2011-03-23 17:56:30
-		
-		ArrayList<CalculatedData> TenMinData = CalculatedDataController.GetAllForPeriod(calStart, calEnd,
-				eCalculatedDataType.TenMinutes);
+		calEnd.set(2011, 2, 25, 18, 00, 00);// 2011-03-23 17:56:30
 
-		
-		
-		List<RawData> rawDataList = RawDataController.GetAllForPeriod(calStart, calEnd);
-		
+		ArrayList<CalculatedData> TenMinData = CalculatedDataController
+				.GetAllForPeriod(calStart, calEnd,
+						eCalculatedDataType.TenMinutes);
+
+		List<RawData> rawDataList = RawDataController.GetAllForPeriod(calStart,
+				calEnd);
+		ArrayList<WorstPylonCase> wcPylonArray = WorstCaseController
+				.GetAllForPeriod(calStart, calEnd, false, false);
+
 		JSONObject obj = null;
-        try {
+		try {
 
-            obj = new JSONObject();
-            
-            JSONArray listOfTimeStamps = new JSONArray();
-            
-            JSONArray listOfWindSpeed = new JSONArray();
-            JSONArray listOfWindSpeed_MAX = new JSONArray();
-            
-            JSONArray listOfWindSpeedDirection = new JSONArray();
-            
-            JSONArray listOfSonarValues = new JSONArray();
-            JSONArray listOfHydrometerValues = new JSONArray();
-            
-            
-            for(int i =0; i< TenMinData.size(); i++ ){
-            	
-            	listOfTimeStamps.put(TenMinData.get(i).getTimestampDate().getTime());
-            	
-            	listOfWindSpeed.put(TenMinData.get(i).getWindSpeed()); //TODO: put the real wind speed values 
-            	listOfWindSpeed_MAX.put(TenMinData.get(i).getWindSpeedMax());
-            	
-            	listOfWindSpeedDirection.put(TenMinData.get(i).getWindDirection());
-            	
-            	listOfSonarValues.put(TenMinData.get(i).getSonar());
-            	listOfHydrometerValues.put(TenMinData.get(i).getHydrometer());
-            }
-           
-            obj.put("Dates", listOfTimeStamps);
-            
-            obj.put("ValuesOfWindSpeed", listOfWindSpeed);
-            obj.put("ValuesOfWindSpeed_MAX", listOfWindSpeed_MAX);
-            
-            obj.put("ValuesOfWindSpeedDirection", listOfWindSpeedDirection);
-            
-            obj.put("ValuesOfSonar", listOfSonarValues);
-            obj.put("ValuesOfHydrometer", listOfHydrometerValues);
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
+			obj = new JSONObject();
+
+			JSONArray listOfTimeStamps = new JSONArray();
+
+			JSONArray listOfWindSpeed = new JSONArray();
+			JSONArray listOfWindSpeed_MAX = new JSONArray();
+
+			JSONArray listOfWindSpeedDirection = new JSONArray();
+
+			JSONArray listOfSonarValues = new JSONArray();
+			JSONArray listOfHydrometerValues = new JSONArray();
+
+			for (int i = 0; i < TenMinData.size(); i++) {
+
+				listOfTimeStamps.put(TenMinData.get(i).getTimestampDate()
+						.getTime());
+
+				listOfWindSpeed.put(TenMinData.get(i).getWindSpeed()); // TODO:
+																		// put
+																		// the
+																		// real
+																		// wind
+																		// speed
+																		// values
+				listOfWindSpeed_MAX.put(TenMinData.get(i).getWindSpeedMax());
+
+				listOfWindSpeedDirection.put(TenMinData.get(i)
+						.getWindDirection());
+
+				listOfSonarValues.put(TenMinData.get(i).getSonar());
+				listOfHydrometerValues.put(TenMinData.get(i).getHydrometer());
+			}
+
+			obj.put("Dates", listOfTimeStamps);
+
+			obj.put("ValuesOfWindSpeed", listOfWindSpeed);
+			obj.put("ValuesOfWindSpeed_MAX", listOfWindSpeed_MAX);
+
+			obj.put("ValuesOfWindSpeedDirection", listOfWindSpeedDirection);
+
+			obj.put("ValuesOfSonar", listOfSonarValues);
+			obj.put("ValuesOfHydrometer", listOfHydrometerValues);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		req.setAttribute("rawDataList", obj);
-		req.setAttribute("modenaPath", ParsedInputFilesController.FetchStoredPath(eFileType.Modena, calEnd));
-		req.setAttribute("mantovaPath", ParsedInputFilesController.FetchStoredPath(eFileType.Mantova, calEnd));
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/tempGraph.jsp");
+		req.setAttribute("modenaPath", ParsedInputFilesController
+				.FetchStoredPath(eFileType.Modena, calEnd));
+		req.setAttribute("mantovaPath", ParsedInputFilesController
+				.FetchStoredPath(eFileType.Mantova, calEnd));
+		req.setAttribute("wcPylonArray", wcPylonArray);
+
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher("/tempGraph.jsp");
 		dispatcher.forward(req, resp);
-		//super.doGet(req, resp);
-		
-		 
+		// super.doGet(req, resp);
+
 	}
 
 	/**
