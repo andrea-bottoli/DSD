29a2f8b8c6a2bd59a42f0ccd8348048ad3ab44dd
Jörn Tillmanns <github@matedealer.de>
2013-12-30 14:01:05 +0100

    First Version of User-Administration. Without Function to add new User

--- Code/src/dsd/dao/UserAccessDAO.java
+++ Code/src/dsd/dao/UserAccessDAO.java
@@ -24,6 +24,10 @@ import dsd.calculations.CryptFunktions;
 import dsd.model.User;
 import dsd.model.enums.eUserRole;
 
+/**
+ * @author joti
+ * 
+ */
 public class UserAccessDAO {
 
 	private static String usertable = "users";
@@ -31,8 +35,12 @@ public class UserAccessDAO {
 	private static String[] usertableFields = new String[] { "ID", "username",
 			"surename", "lastname", "password", "email" };
 	private static String[] roleTableFields = new String[] { "ID", "username",
-			"role" };
+			"role", "userID" };
 
+	/**
+	 * @param username
+	 * @return
+	 */
 	public static User selectUserByUsername(String username) {
 
 		ArrayList<User> userList = selectUser(usertableFields[1], username);
@@ -43,18 +51,29 @@ public class UserAccessDAO {
 
 	}
 
+	/**
+	 * @param username
+	 * @return
+	 */
 	public static ArrayList<User> selectUserByLastname(String username) {
 
 		return selectUser(usertableFields[3], username);
 
 	}
 
+	/**
+	 * @param username
+	 * @return
+	 */
 	public static ArrayList<User> selectUserByFirstname(String username) {
 
 		return selectUser(usertableFields[2], username);
 
 	}
 
+	/**
+	 * @return
+	 */
 	public static ArrayList<User> getAllUsers() {
 		ArrayList<User> userList = new ArrayList<User>();
 
@@ -79,7 +98,9 @@ public class UserAccessDAO {
 				user.setRole(eUserRole.valueOf(result
 						.getString(roleTableFields[2])));
 				userList.add(user);
+				user.setID(result.getInt(usertableFields[0]));
 			}
+			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -87,6 +108,11 @@ public class UserAccessDAO {
 		return userList;
 	}
 
+	/**
+	 * @param field
+	 * @param param
+	 * @return
+	 */
 	private static ArrayList<User> selectUser(String field, String param) {
 		try {
 			ArrayList<User> userList = new ArrayList<User>();
@@ -105,6 +131,7 @@ public class UserAccessDAO {
 			System.out.println(result.getStatement().toString());
 			while (result.next()) {
 				User user = new User();
+				user.setID(result.getInt(usertableFields[0]));
 				user.setUsername(result.getString(usertableFields[1]));
 				user.setSurename(result.getString(usertableFields[2]));
 				user.setLastname(result.getString(usertableFields[3]));
@@ -114,6 +141,7 @@ public class UserAccessDAO {
 						.getString(roleTableFields[2])));
 				userList.add(user);
 			}
+			con.close();
 			return userList;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -123,10 +151,76 @@ public class UserAccessDAO {
 		return null;
 	}
 
-	public static void setUser(User user) {
+	/**
+	 * @param user
+	 */
+	public static void saveUser(User user) {
+		try {
+			Connection con = DAOProvider.getDataSource().getConnection();
+			String table = usertable;
+			String[] fields = { usertableFields[1], usertableFields[2],
+					usertableFields[3], usertableFields[4], usertableFields[5] };
+			Object[] valueArray = { user.getUsername(), user.getSurename(),
+					user.getLastname(), user.getPasswd(), user.getEmail() };
+
+			String where = "ID = ?";
+			Object[] wherePartParameters = { user.getID() };
+
+			DAOProvider.UpdateRowSecure(table, fields, where, con, valueArray,
+					wherePartParameters);
+
+			String table_b = roleTable;
+			String[] fields_b = { roleTableFields[1], roleTableFields[2] };
+			Object[] valueArray_b = { user.getUsername(),
+					user.getRole().toString() };
+
+			DAOProvider.UpdateRowSecure(table_b, fields_b, where, con,
+					valueArray_b, wherePartParameters);
+
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
+	/**
+	 * @param user
+	 */
+	public static void newUser(User user) {
+		try {
+			Connection con = DAOProvider.getDataSource().getConnection();
+
+			String fields = usertableFields[1] + "," + usertableFields[2] + ","
+					+ usertableFields[3] + "," + usertableFields[4] + ","
+					+ usertableFields[5];
+			Object[] valueArray = { user.getUsername(), user.getSurename(),
+					user.getLastname(), user.getPasswd(), user.getEmail() };
+
+			int ID = DAOProvider.InsertRowSecure(usertable, fields, con,
+					valueArray);
+
+			fields = roleTableFields[1] + "," + roleTableFields[2] + ","
+					+ roleTableFields[3];
+			Object[] valueArray_roles = { user.getUsername(),
+					user.getRole().toString(), ID };
+
+			DAOProvider.InsertRowSecure(roleTable, fields, con,
+					valueArray_roles);
+
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	@SuppressWarnings("unused")
 	public static boolean checkPassword(String username, String password) {
 		String cryptedPassword = CryptFunktions.MD5(password);
@@ -137,6 +231,7 @@ public class UserAccessDAO {
 			ResultSet result = DAOProvider.SelectTableSecure(usertable,
 					usertableFields[4], usertableFields[1] + " = ? ", "", con,
 					param);
+			con.close();
 			return true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -146,6 +241,10 @@ public class UserAccessDAO {
 		return false;
 	}
 
+	/**
+	 * @param username
+	 * @return
+	 */
 	public static boolean deletUser(String username) {
 		Object[] param = { username };
 		try {
@@ -155,6 +254,7 @@ public class UserAccessDAO {
 					roleTableFields[1] + " = ?", con, param);
 			int result2 = DAOProvider.DeleteRowSecure(usertable,
 					usertableFields[1] + " = ?", con, param);
+			con.close();
 			if (result1 != 0 && result2 != 0)
 				return true;
 		} catch (SQLException e) {
